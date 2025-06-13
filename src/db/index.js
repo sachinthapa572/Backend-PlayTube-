0d58ceddb6167badc0a5397341737a836f227879
sachinthapa572 <121659364+sachinthapa572@users.noreply.github.com>
2025-06-13 17:22:57 +0545

    update the mistake dont look the previous commit

--- src/db/index.js
+++ src/db/index.js
@@ -1,10 +1,10 @@
 import mongoose from 'mongoose';
-
-const MONGO_URL =
-	'mongodb+srv://adminadmin:BGz7LXh0nsftg0fX@project.mzyde.mongodb.net';
+import { ApiError } from '../utils/ApiError.js';
 const connectdb = async () => {
 	try {
-		const connectionInstance = await mongoose.connect(MONGO_URL);
+		const connectionInstance = await mongoose.connect(
+			`${process.env.MOGO_URI}/${process.env.MOGO_DB_NAME}`
+		);
 		console.log(
 			` MongoDB connected !! DB Name:${connectionInstance.connection.name}`
 		);
